7a6f7c7f84b57b74c5a6091db6f4a42905025244
Infocat <infocat2.0@gmail.com>
2017-12-28 16:41:52 -0500

    0.12

--- registro.php
+++ registro.php
@@ -3,88 +3,103 @@
 	<head>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="css/bootstrap.min.css">
-		<link rel="stylesheet" href="css/estilosp.css?version=1.0">
+		<link rel="stylesheet" href="css/estilosp.css?version=1.0.1">
 		<link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
 		<script src='https://www.google.com/recaptcha/api.js'></script>
+		<title>Registro: Criptocash - Bienvenido</title>
+		<link rel="stylesheet" href="css/font-awesome.min.css">
 
 	</head>
 </html>
 <body>
-	<style type="text/css">
-		.panel-default>.panel-heading { border-color:transparent!important; }
-		.iconoMovible{float: left; color: #ccc; padding-left: 20px}
-		.form-group input{}
-		.panel-heading {    border-top-left-radius: 17px;
-    						border-top-right-radius: 17px;
-}
-		.panel   {border-radius: 17px;}
-
-		
-	</style>
+<style type="text/css">
+	
+	.panel-body{padding-top: 0px;}
+	.panel-default>.panel-heading { border-color:transparent!important; }
+	.iconoMovible{color: #ccc; padding-left: 20px;     top: 33px;    position: relative;    left: 44px; font-size: 17px;}
+	.form-group { margin-bottom: -2px; }
+	.form-group input{padding-left: 47px;}
+	.panel-title{font-size: 30px}
+	.c-checkbox{font-weight: 500;}
+	.btn-primary{
+		background-color: #0078D7!important;
+   		border-color: #056fc3!important;
+	}
+	a{color: #0078D7;}
+	.form-control  {
+    	transition: all 0.8s;
+	}
+	.form-control:focus {
+    	border-color: #FFEB3B;
+    	box-shadow: inset 0 1px 1px rgba(245, 208, 27, 0.26), 0 0 6px rgba(255, 193, 7, 0.41);
+    	transition: all 0.8s;
+	}
+</style>
+<main>
+</main>
 	<header id="header">
 		
 	</header>
 	<div id="cuerpo">	
 		<div class="container" id="divmedio">
-        <div class="row centered-form"  >
-        <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-3 ">
-        	<div class="panel panel-default" style=" box-shadow:0px 2px 20px 7px rgba(0,0,0,.05); height: 45em; width: 35em;">
-        		<div class="panel-heading" style="background:#fff; padding-top: 50px"">
-			    		<h3 id="title" class="panel-title text-center" style="font-size:38px ; padding-top: 5px; color: #808080">Regístrate</h3>
-			    </div>
+		<div class="row centered-form"  >
+		<div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-3 ">
+			<div class="panel panel-default" style="box-shadow: 0px 2px 12px 7px rgba(0,0,0,.05); height: 45em; width: 35em;">
+				<div class="panel-heading" style="background:#fff; padding-top: 50px"">
+						<h3 id="title" class="panel-title text-center" style="padding-top: 5px; ">Bienvenido a Criptocash</h3>
+				</div>
+						<div class="panel-body" >
+							<div class="form-group ">
+								<span class="iconoMovible"><i class="fa fa-user" aria-hidden="true"></i></span>
+								<input type="email" style="height: 45px; width: 80%; margin: 0 auto;" name="email" id="txtemail" class="form-control input-sm " placeholder="Correo Electrónico">
+							</div>
+												
+							<div class="form-group ">
+								<span class="iconoMovible"><i class="fa fa-lock" aria-hidden="true"></i></span>
+								<input type="password" style="height: 45px; width: 80%; margin: 0 auto;" name="password" id="txtpassword" class="form-control input-sm" placeholder="Contraseña">
+							</div>
+																
+							<div class="form-group ">
+								<span class="iconoMovible"><i class="fa fa-lock" aria-hidden="true"></i></span>
+								<input type="password" style="height: 45px; width: 80%; margin: 0 auto;" name="password_confirmation" id="txtrepetir_password" class="form-control input-sm" placeholder="Confirmar Contraseña">
+							</div>
 
-			 			<div class="panel-body col-sm-offset-1 col-md-offset-0 col-xs-offset-1 " >
-			    		
+							<div class="col-xs-offset-1 col-md-offset-2" >
+								<div class="g-recaptcha" style="padding-top: 20px;" data-sitekey="6LcD8jwUAAAAAOyLgujlyxs9jGpczeONwQl0gk8X"></div>
+							</div>	
 
-			    			<div class="form-group col-md-offset-1">
-			    				<span class="iconoMovible"><i class="glyphicon glyphicon-user"></i></span>
-			    				<input type="email" style="height: 45px; width: 340px;" name="email" id="txtemail" class="form-control input-sm " placeholder="Correo Electrónico">
-			    			</div>
-			    			    				
-			    			<div class="form-group col-md-offset-1">
-			    				<span class="iconoMovible"><i class="glyphicon glyphicon-tag"></i></span>
-			    				<input type="password" style="height: 45px; width: 340px" name="password" id="txtpassword" class="form-control input-sm" placeholder="Contraseña">
-			    			</div>
-			    							    				
-			    			<div class="form-group col-md-offset-1">
-			    				<span class="iconoMovible"><i class="glyphicon glyphicon-tags"></i></span>
-			    				<input type="password" style="height: 45px; width: 340px" name="password_confirmation" id="txtrepetir_password" class="form-control input-sm" placeholder="Confirmar Contraseña">
-			    			</div>
+								<hr>
 
-			    			<div class="col-md-offset-2 col-sm-offset-1 col-xs-offset-1" >
-			    				<div class="g-recaptcha" style="padding-top: 20px;" data-sitekey="6LcD8jwUAAAAAOyLgujlyxs9jGpczeONwQl0gk8X"></div>
-			    			</div>	
+							<div>
+								<label class="c-checkbox col-xs-offset-2" style="padding-bottom: 15px;">
+									 <input type="checkbox" id="chkterminos" />
+									 <span class="text" style="font-size: 11px;">
+										He leído y acepto los <a href="">Términos y condiciones</a>
+									</span>
+								</label>
 
-			    				<hr>
+								<input type="submit" id="btncontinuar" style="height: 45px; width:80%; margin:0 auto; " value="Continuar" class="btn btn-primary btn-block col-md-offset-1">
 
-			    			<div>
-			    					<label class="c-checkbox col-md-offset-3 col-sm-offset-2 col-xs-offset-2" style="padding-bottom: 15px;">
-			    						 <input type="checkbox" id="chkterminos" />
-			    						 <span class="text" style="font-size: 11px;">
-			    							He leído y acepto los <a href="">Términos y condiciones</a>
-			    						</span>
-			    					</label>
-						    						    			
-					    			<input type="submit" id="btncontinuar" style="height: 45px; width:380px;" value="Continuar" class="btn btn-primary btn-block col-md-offset-1">
-
-					    			<div style="padding-top: 30px;">
-					    				<h5 class="text-center">¿Ya tiene una cuenta? <a href="login.html">Iniciar Sesión</a></h5>
-					    			</div>
-			    			</div>
-			    		</div>
-	    	</div>
-    	</div>
-    </div>
-    </div>
-    </div>
-    <script
-  src="https://code.jquery.com/jquery-2.2.4.min.js"
+								<div style="padding-top: 30px;">
+									<h5 class="text-center">¿Ya posee una cuenta? <a href="">Inicie sesión aquí</a></h5>
+								</div>
+							</div>
+						</div>
+			</div>
+		</div>
+	</div>
+	</div>
+	</div>
+<script   src="https://code.jquery.com/jquery-2.2.4.min.js"
   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
   crossorigin="anonymous"></script>
 <script type="text/javascript">
-	$("#btncontinuar").click(function () {
-		console.log("click en el btncontinuar");
-		window.location.href="index.html"
-	});
-  </script>
+$(document).ready(function () {
+	$('#txtemail').focus();
+});
+$("#btncontinuar").click(function () {
+	console.log("click en el btn");
+	window.location.href="index.html"
+});
+</script>
 </body>
